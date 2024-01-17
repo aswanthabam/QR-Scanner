c4acc4c626a3c571a07c7147b441d080107c58b6
Aswanth Vc <aswanth.abam@gmail.com>
2024-01-17 23:46:52 +0530

    fix : switch statment for navigation to if condidtion

--- app/src/main/java/com/abam/qrscanner/MainActivity.java
+++ app/src/main/java/com/abam/qrscanner/MainActivity.java
@@ -3,9 +3,8 @@ package com.abam.qrscanner;
 import android.Manifest;
 import android.view.MenuItem;
 import android.os.Bundle;
-import android.widget.*;
-import android.view.*;
 import android.content.pm.*;
+import com.abam.qrscanner.R;
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -26,8 +25,8 @@ public class MainActivity extends AppCompatActivity
 	private NavigationView mNav;
 	private ActionBarDrawerToggle mToogle;
 	private androidx.appcompat.widget.Toolbar mToolbar;
-	
-	
+
+	public final static int drawer_navigation_item1 = R.id.drawer_navigation_item1;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -66,61 +65,40 @@ public class MainActivity extends AppCompatActivity
 	}
 	
 	public void itemSelection(int item){
-		switch(item){
-			case android.R.id.home:
-				mDrawer.openDrawer(GravityCompat.START);
-				break;
-			case R.id.drawer_navigation_item1:
-			case R.id.navigation_item1:
-				bottomNav.getMenu().findItem(R.id.navigation_item1).setChecked(true);
-				//bottomNav.getMenu().findItem(R.id.navigation_item2).setChecked(false);
-				//bottomNav.getMenu().findItem(R.id.navigation_item3).setChecked(false);
-				//bottomNav.getMenu().findItem(R.id.navigation_item4).setChecked(false);
-				Fragment frag1;
-				FragmentTransaction transaction1 = getSupportFragmentManager().beginTransaction();
-				frag1 = new HomeFragmet(this);
-				transaction1.replace(R.id.frame_container,frag1);
-				transaction1.commit();
-				break;
-			case R.id.drawer_navigation_item2:
-			case R.id.navigation_item2:
-				Fragment frag2;
-				FragmentTransaction transaction2 = getSupportFragmentManager().beginTransaction();
-				//bottomNav.getMenu().findItem(R.id.navigation_item1).setChecked(false);
-				bottomNav.getMenu().findItem(R.id.navigation_item2).setChecked(true);
-				//bottomNav.getMenu().findItem(R.id.navigation_item3).setChecked(false);
-				//bottomNav.getMenu().findItem(R.id.navigation_item4).setChecked(false);
-				
-				frag2 = new QRscanner(this);
-				transaction2.replace(R.id.frame_container,frag2);
-				transaction2.commit();
-				break;
-			case R.id.drawer_navigation_item3:
-			case R.id.navigation_item3:
-				Fragment frag3;
-				FragmentTransaction transaction3 = getSupportFragmentManager().beginTransaction();
-				//bottomNav.getMenu().findItem(R.id.navigation_item1).setChecked(false);
-				//bottomNav.getMenu().findItem(R.id.navigation_item2).setChecked(false);
-				bottomNav.getMenu().findItem(R.id.navigation_item3).setChecked(true);
-				//bottomNav.getMenu().findItem(R.id.navigation_item4).setChecked(false);
-				
-				frag3 = new QRgenerator(this);
-				transaction3.replace(R.id.frame_container,frag3);
-				transaction3.commit();
-				break;
-			case R.id.drawer_navigation_item4:
-			case R.id.navigation_item4:
-				Fragment frag4;
-				FragmentTransaction transaction4 = getSupportFragmentManager().beginTransaction();
-				//bottomNav.getMenu().findItem(R.id.navigation_item1).setChecked(false);
-				//bottomNav.getMenu().findItem(R.id.navigation_item2).setChecked(false);
-				//bottomNav.getMenu().findItem(R.id.navigation_item3).setChecked(false);
-				bottomNav.getMenu().findItem(R.id.navigation_item4).setChecked(true);
-				
-				frag4 = new AboutFragmet(this);
-				transaction4.replace(R.id.frame_container,frag4);
-				transaction4.commit();
-				break;
+
+//		final int navigation_item1 = R.id.navigation_item1
+		if(item == android.R.id.home) mDrawer.openDrawer(GravityCompat.START);
+		else if (item == R.id.drawer_navigation_item1 || item == R.id.navigation_item1) {
+			bottomNav.getMenu().findItem(R.id.navigation_item1).setChecked(true);
+			Fragment frag1;
+			FragmentTransaction transaction1 = getSupportFragmentManager().beginTransaction();
+			frag1 = new HomeFragmet(this);
+			transaction1.replace(R.id.frame_container,frag1);
+			transaction1.commit();
+		}
+		else if(item == R.id.drawer_navigation_item2 || item == R.id.navigation_item2) {
+			Fragment frag2;
+			FragmentTransaction transaction2 = getSupportFragmentManager().beginTransaction();
+			bottomNav.getMenu().findItem(R.id.navigation_item2).setChecked(true);
+			frag2 = new QRscanner(this);
+			transaction2.replace(R.id.frame_container,frag2);
+			transaction2.commit();
+		}
+		else if (item == R.id.drawer_navigation_item3 || item == R.id.navigation_item3) {
+			Fragment frag3;
+			FragmentTransaction transaction3 = getSupportFragmentManager().beginTransaction();
+			bottomNav.getMenu().findItem(R.id.navigation_item3).setChecked(true);
+			frag3 = new QRgenerator(this);
+			transaction3.replace(R.id.frame_container,frag3);
+			transaction3.commit();
+		}
+		else if(item == R.id.drawer_navigation_item4 || item == R.id.navigation_item4) {
+			Fragment frag4;
+			FragmentTransaction transaction4 = getSupportFragmentManager().beginTransaction();
+			bottomNav.getMenu().findItem(R.id.navigation_item4).setChecked(true);
+			frag4 = new AboutFragmet(this);
+			transaction4.replace(R.id.frame_container,frag4);
+			transaction4.commit();
 		}
 	}
 	
